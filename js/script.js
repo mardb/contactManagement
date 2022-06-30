611079da394bd8068486b8ffd3fe245dff43345c
Marlene Guzman <guzmanmarlene@yahoo.com>
2022-06-30 12:04:40 -0700

    starting EC

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-const url = 'https://randomuser.me/api/?nat=us&results=12';
+const url = 'https://randomuser.me/api/?nat=us&results=12&inc=name,location,email,dob,cell,picture';
 const body = document.querySelector('body');
 const gallery = document.querySelector('.gallery');
+
 let people = [];
 
 fetch(url)
@@ -13,6 +14,7 @@ fetch(url)
 
 const generateIcons = (data) => {
   const items = data.forEach((item, index) => {
+
     const htmlcard = `
   <div class="card" data-index=${index}>
   <div class="card-img-container">
@@ -29,31 +31,27 @@ const generateIcons = (data) => {
     gallery.insertAdjacentHTML('beforeend', htmlcard);
   });
 
-  [...document.getElementsByClassName('card')].forEach((card) => {
-    card.addEventListener('click', (e) => {
-      let dataIndex = card.getAttribute('data-index')
-      //people[dataIndex] is an object from people array plus or index of (data-index) fron foear each index that i ppassed.
-      //generateModal is passing in the information from person with certain index and then the modal of that ppops up 
-     generateModal(people[dataIndex])
-    });
-  });
+  modalOpen();
 };
 
 //singel person is JUST a name, it means nothing
 const generateModal = (singlePerson) => {
-    const htmlModal = `
+  const isoDate = singlePerson.dob.date.split('T')[0].split('-');
+  const birthday = `${isoDate[1]}/${isoDate[2]}/${isoDate[0]}`;
+
+  const htmlModal = `
       <div class="modal-container">
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
                         <img class="modal-img" src="${singlePerson.picture.thumbnail}" alt="profile picture">
                         <h3 id="name" class="modal-name cap">${singlePerson.name.first} ${singlePerson.name.last}</h3>
-                        <p class="modal-text">${singlePerson.email}</p>
+                        <p class="modal-text">${singlePerson.email}</p> 
                         <p class="modal-text cap">${singlePerson.location.city}</p>
                         <hr>
-                        <p class="modal-text">${singlePerson.phone}</p>
-                        <p class="modal-text">${singlePerson.phone}123 Portland Ave., Portland, OR 97204</p>
-                        <p class="modal-text">Birthday: 10/21/2015</p>
+                        <p class="modal-text">${singlePerson.cell}</p>
+                        <p class="modal-text">${singlePerson.location.street.number} ${singlePerson.location.street.name}, ${singlePerson.location.city}, ${singlePerson.location.state} ${singlePerson.location.postcode}</p>
+                        <p class="modal-text">Birthday: ${birthday}</p>
                     </div>
                 </div>
 
@@ -64,14 +62,28 @@ const generateModal = (singlePerson) => {
                 </div>
             </div>
       `;
-    gallery.insertAdjacentHTML('beforeend', htmlModal);
+  gallery.insertAdjacentHTML('beforeend', htmlModal);
 
+  modalClose();
+};
+const modalOpen = () => {
+  [...document.getElementsByClassName('card')].forEach((card) => {
+    card.addEventListener('click', (e) => {
+      let dataIndex = card.getAttribute('data-index');
+      //people[dataIndex] is an object from people array plus or index of (data-index) fron foear each index that i ppassed.
+      //generateModal is passing in the information from person with certain index and then the modal of that ppops up
+      generateModal(people[dataIndex]);
+      nextButton();
+    });
+  });
 };
 
-//eventlistener
+const modalClose = () => {
+  const closeBtn = document.querySelector('#modal-close-btn');
+  const modal = document.querySelector('.modal-container');
+  closeBtn.addEventListener('click', (e) => {
+    modal.remove();
+  });
+};
 
 
-/*
- *NOTE: When adding or concatenating to the DOM, avoid doing this: element.innerHTML += 'HTML string'. That technique rewrites the entire DOM. This is problematic because any rewritten elements won't retain any event listeners that were attached to the DOM before the rewrite occurs. Use this method instead: element.insertAdjacentHTML('beforeend', 'HTML string'). That technique will allow you to add strings of HTML to the DOM without disrupting what already exists in the DOM.
- */
-// ------------------ javascript
